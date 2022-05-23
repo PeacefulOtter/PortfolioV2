9bc8191a5e4eb27ca1a7184abf8a05121bb70bb5
PeacefulOtter <zankro67@gmail.com>
2022-05-23 11:01:13 +0200

    More projects showcased

--- Components/Projects/Projects.tsx
+++ Components/Projects/Projects.tsx
@@ -13,8 +13,36 @@ interface IProject {
 }
 
 const projects: IProject[] = [
-    { repo: 'SelfDrivingCar', desc: '2D car simulation that learns to drive around a track using a neural network and genetic algorithm. This project has been built from scratch using the least libraries possible (JavaFX). ', tags: ['machine-learning', 'neural-network', 'genetic-algorithm'], links: [] },
-    { repo: 'Weaver', desc: 'A spotify like app with an improved UI / UX, made in NextJS ', tags: ['Spotify', 'NextJS', 'React', 'UI / UX'], links: [] }
+    { 
+        repo: 'SelfDrivingCar', 
+        desc: '2D car simulation that learns to drive around a track using a neural network and genetic algorithm. This project has been built from scratch using the least libraries possible (JavaFX).', 
+        tags: ['machine-learning', 'neural-network', 'genetic-algorithm'], 
+        links: [] 
+    },
+    { 
+        repo: '3DGameEngine', 
+        desc: '3D Game Engine built from scratch using LWJGL (OpenGL) in Java. It is capable of loading and rendering 3D objects with textures, normals and height maps. The engine also supports multiple shaders as well as ambient, directional, point and spot lights.', 
+        tags: ['game-engine', 'lwjgl', 'opengl', 'shaders', '3d-game'], 
+        links: [] 
+    },
+    { 
+        repo: 'ForceFeedback', 
+        desc: 'A force feedback controller sending inputs to a steering wheel through a web interface. The frontend requests a Java backend made with Spring and connected to a controller.', 
+        tags: ['force-feedback', 'steering-wheel', 'react', 'java-spring'], 
+        links: [] 
+    },
+    { 
+        repo: 'Mintel', 
+        desc: 'A 5-stage pipelined RISC microprocessor written in Scala using Chisel. Data dependencies are resolved using data forwarding and stalling occurs on a branch / jump. Mintel can be programmed on any FPGA and supports IOs', 
+        tags: ['risc', 'mips', 'pipeline', 'chisel', 'microprocessor', 'fpga'], 
+        links: [] 
+    },
+    { 
+        repo: 'Weaver', 
+        desc: 'A spotify like app with an improved UI / UX, made in NextJS', 
+        tags: ['spotify', 'nextjs', 'ui', 'ux'], 
+        links: [] 
+    },
 ]
 
 const img = (repo: string) => `https://raw.githubusercontent.com/PeacefulOtter/${repo}/main/showcase.png`
@@ -34,9 +62,9 @@ const Project = ( { repo, desc, tags, links }: IProject ) => {
                     </div>
                 </div>
                 <div className={styles.links}>
-                    <div className={styles.link}><FiGithub /></div>
+                    <a className={styles.link} href={`https://www.github.com/PeacefulOtter/${repo}`} target="_blank"><FiGithub /></a>
                     { links.map( (link, i) => 
-                        <div key={`link${i}`} className={styles.link}>{link}</div>
+                        <a key={`link${i}`} href={link} className={styles.link} target="_blank">{link}</a>
                     ) }
                 </div>
             </div>
